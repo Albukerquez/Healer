6b33a6eef8480c476dab231bdd4eeff425f2cc9a
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-04-23 20:41:26 +0500

    finished tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,13 +18,22 @@
 class Post < ApplicationRecord
   acts_as_taggable # Alias for acts_as_taggable_on :tags
 
+  list = lambda do |page, tag|
+    recent_paginated(page).with_tag(tag)
+  end
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   belongs_to :author
 
+  PER_PAGE = 6
+
   scope :most_recent, (-> { order(published_at: :desc) })
   scope :published, (-> { where(published: true) })
+  scope :recent_paginated, (->(page) { most_recent.paginate(page: page, per_page: PER_PAGE) })
+  scope :with_tag, (->(tag) { tagged_with(tag) if tag.present? })
+  scope :list_for, list
 
   def should_generate_new_friendly_id?
     title_changed?
