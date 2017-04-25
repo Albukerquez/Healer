5fcca056847e162d22cccbf80c6683498ea3ebc8
Eugene Schwartz <nonsenseftw@yandex.ru>
2017-04-25 07:43:50 +0500

    account

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'blog/posts#index'
 
   namespace :authors do
+    get '/account' => 'accounts#edit', as: :account
+    put '/info' => 'accounts#update_info', as: :info
+    put '/password' => 'accounts#update_password', as: :password
     resources :posts do
       put 'publish' => 'posts#publish', on: :member
       put 'unpublish' => 'posts#unpublish', on: :member
